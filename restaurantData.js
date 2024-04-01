35161341fa791070b9df9e5573fc3cb60573374c
Ananya Mohapatra <ananyamohapatra27@gmail.com>
2024-04-02 00:41:15 +0530

    added restaurant json

---
+++ restaurantData.js
@@ -0,0 +1,792 @@
+const resObj= [
+    {
+    "id": "17036",
+    "name": "Leon's - Burgers & Wings (Leon Grill)",
+    "cloudinaryImageId": "b2edbc28b7b8219d6e0a9c049ce06658",
+    "locality": "HSR Layout",
+    "areaName": "HSR Layout",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+      "American",
+      "Snacks",
+      "Turkish",
+      "Portuguese",
+      "Continental"
+    ],
+    "avgRating": 4.4,
+    "parentId": "371281",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 19,
+      "lastMileTravel": 0.7,
+      "serviceability": "SERVICEABLE",
+      "slaString": "15-20 mins",
+      "lastMileTravelString": "0.7 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-02 04:00:00",
+      "opened": true
+    },
+    "badges": {
+      
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "₹125 OFF",
+      "subHeader": "ABOVE ₹199",
+      "discountTag": "FLAT DEAL"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },
+  {
+    "id": "2675",
+    "name": "Subway",
+    "cloudinaryImageId": "63178e3e64d503a479f2a2048a474552",
+    "locality": "7th Sector",
+    "areaName": "Hsr Layout",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+      "Salads",
+      "Snacks",
+      "Desserts",
+      "Beverages"
+    ],
+    "avgRating": 4.1,
+    "parentId": "2",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 30,
+      "lastMileTravel": 0.4,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "0.4 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-02 03:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Sandwiches.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Sandwiches.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "50% OFF",
+      "subHeader": "UPTO ₹100"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },{
+ 
+      "id": "23682",
+      "name": "McDonald's",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/d2116933-061e-455a-8b98-de8da69e3ba2_23682.jpg",
+      "locality": "HSR Layout",
+      "areaName": "HSR Layout",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Burgers",
+        "Beverages",
+        "Cafe",
+        "Desserts"
+      ],
+      "avgRating": 4.3,
+      "parentId": "630",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "10K+",
+      "sla": {
+        "deliveryTime": 21,
+        "lastMileTravel": 0.4,
+        "serviceability": "SERVICEABLE",
+        "slaString": "20-25 mins",
+        "lastMileTravelString": "0.4 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-04-02 06:00:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "Rxawards/_CATEGORY-Burger.png",
+            "description": "Delivery!"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "Delivery!",
+                  "imageId": "Rxawards/_CATEGORY-Burger.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "40% OFF",
+        "subHeader": "UPTO ₹80"
+      },
+      "loyaltyDiscoverPresentationInfo": {
+        "logoCtx": {
+          "text": "BENEFITS",
+          "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+        },
+        "freedelMessage": "FREE DELIVERY"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+    
+  },
+  {
+    "id": "57283",
+    "name": "Burger King",
+    "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    "locality": "HSR Layout",
+    "areaName": "Hsr Layout",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+      "Burgers",
+      "American"
+    ],
+    "avgRating": 4.2,
+    "parentId": "166",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 25,
+      "lastMileTravel": 0.4,
+      "serviceability": "SERVICEABLE",
+      "slaString": "20-25 mins",
+      "lastMileTravelString": "0.4 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-02 03:50:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Burger.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Burger.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "ITEMS",
+      "subHeader": "AT ₹139"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },{
+    "id": "18973",
+    "name": "Nandhana Palace",
+    "cloudinaryImageId": "195876a3181ef63f76e45e3a7b49b585",
+    "locality": "Koramangala",
+    "areaName": "Koramangala",
+    "costForTwo": "₹500 for two",
+    "cuisines": [
+      "Biryani",
+      "Andhra",
+      "South Indian",
+      "North Indian"
+    ],
+    "avgRating": 4.2,
+    "parentId": "2120",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 33,
+      "lastMileTravel": 4.2,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "4.2 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-02 02:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Andhra.png",
+          "description": "Delivery!"
+        },
+        {
+          "imageId": "Rxawards/_CATEGORY-Biryani.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Andhra.png"
+              }
+            },
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Biryani.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },{
+    "id": "17310",
+    "name": "KFC",
+    "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
+    "locality": "HSR Layout",
+    "areaName": "HSR Layout",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+      "Burgers",
+      "Biryani",
+      "American",
+      "Snacks",
+      "Fast Food"
+    ],
+    "avgRating": 4.3,
+    "parentId": "547",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 22,
+      "lastMileTravel": 0.6,
+      "serviceability": "SERVICEABLE",
+      "slaString": "20-25 mins",
+      "lastMileTravelString": "0.6 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-02 00:00:00",
+      "opened": true
+    },
+    "badges": {
+      
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "30% OFF",
+      "subHeader": "UPTO ₹75"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },{
+    "id": "10581",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+    "locality": "HSR Layout",
+    "areaName": "HSR Layout",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+      "Pizzas"
+    ],
+    "avgRating": 4.1,
+    "parentId": "721",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 25,
+      "lastMileTravel": 0.6,
+      "serviceability": "SERVICEABLE",
+      "slaString": "25-30 mins",
+      "lastMileTravelString": "0.6 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-02 01:00:00",
+      "opened": true
+    },
+    "badges": {
+      
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "50% OFF",
+      "subHeader": "UPTO ₹100"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },{
+    "id": "23805",
+    "name": "Domino's Pizza",
+    "cloudinaryImageId": "d0450ce1a6ba19ea60cd724471ed54a8",
+    "locality": "1st Sector",
+    "areaName": "HSR Layout",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+      "Pizzas",
+      "Italian",
+      "Pastas",
+      "Desserts"
+    ],
+    "avgRating": 4.4,
+    "parentId": "2456",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 25,
+      "lastMileTravel": 2,
+      "serviceability": "SERVICEABLE",
+      "slaString": "25 mins",
+      "lastMileTravelString": "2.0 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-01 23:59:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Pizza.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Pizza.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "30% OFF",
+      "subHeader": "UPTO ₹75"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },{
+    "id": "28981",
+    "name": "Baskin Robbins - Ice Cream Desserts",
+    "cloudinaryImageId": "85ccae4e3576f9330af102c46ca85395",
+    "locality": "2nd Sector",
+    "areaName": "HSR Layout",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+      "Desserts"
+    ],
+    "avgRating": 4.6,
+    "veg": true,
+    "parentId": "5588",
+    "avgRatingString": "4.6",
+    "totalRatingsString": "1K+",
+    "sla": {
+      "deliveryTime": 18,
+      "lastMileTravel": 1.5,
+      "serviceability": "SERVICEABLE",
+      "slaString": "15-20 mins",
+      "lastMileTravelString": "1.5 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2024-04-01 23:30:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "v1695133679/badges/Pure_Veg111.png",
+          "description": "pureveg"
+        }
+      ],
+      "textExtendedBadges": [
+        {
+          "iconId": "v1705582451/Ratnesh_Badges/Listing_HR.png",
+          "shortDescription": "Perfect Cake Delivery",
+          "fontColor": "#7E808C"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "pureveg",
+                "imageId": "v1695133679/badges/Pure_Veg111.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+          
+        },
+        "textExtendedBadges": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "",
+                "fontColor": "#7E808C",
+                "iconId": "v1705582451/Ratnesh_Badges/Listing_HR.png",
+                "shortDescription": "Perfect Cake Delivery"
+              }
+            }
+          ]
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "₹125 OFF",
+      "subHeader": "ABOVE ₹199",
+      "discountTag": "FLAT DEAL"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+      "logoCtx": {
+        "text": "BENEFITS",
+        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+      },
+      "freedelMessage": "FREE DELIVERY"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {
+          
+        },
+        "video": {
+          
+        }
+      }
+    },
+    "reviewsSummary": {
+      
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+      
+    }
+  },]
+
+export default resObj
